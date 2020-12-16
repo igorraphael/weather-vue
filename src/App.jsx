ea7820d5da1104ac9bbd1307538481387a9c7957
Igor Raphael <igorraphaeel@live.com>
2020-12-16 13:04:17 -0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logoV from './assets/logo.png'
+
+export default {
+
+    render() {
+
+        return (
+            <div id="main">
+                <h1>A simple weather app with vue3</h1>
+                <img src={logoV} alt='img' />
+            </div>
+        )
+    }
+}
