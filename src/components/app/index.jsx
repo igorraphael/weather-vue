1bd0065dd68b88be9be21e07d187b6da38867d89
Igor Raphael <igorraphaelma@gmail.com>
2021-02-22 17:49:41 -0300

    fix

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -4,9 +4,37 @@ import pt_BR from 'ant-design-vue/es/locale-provider/pt_BR'
 import { defineComponent } from 'vue'
 import Header from './header'
 import './index.less'
-import city from '@/assets/city.svg';
+
 import InlineSvg from 'vue-inline-svg'
-import { PlusCircleOutlined, PlusCircleFilled, PlusCircleTwoTone } from '@ant-design/icons-vue'
+import city from '@/assets/city.svg'
+import { PlusCircleTwoTone } from '@ant-design/icons-vue'
+
+const NewCity = defineComponent({
+
+    name: 'NewCity',
+    setup() {
+
+        const propsIcon = {
+
+            src: '/src/assets/city.svg',
+            width: '10em',
+            height: '10em'
+        }
+
+        return () => (
+            <a-card class="card-new-city">
+                <h2>Add city</h2>
+
+                <a-button class="btn-add" block onClick={() => alert('aaa')} type="link" v-slots={{
+                    icon: () => <PlusCircleTwoTone style={{ fontSize: '4em' }} twoToneColor="#16d9ad" />
+                }}>
+
+                </a-button>
+                <InlineSvg {...propsIcon} />
+            </a-card>
+        )
+    }
+})
 
 export default defineComponent({
 
@@ -27,30 +55,13 @@ export default defineComponent({
 
                 <div id="container">
                     <a-row justify="center" gutter={[18, 18]}>
-                        {/* {
-                            [...Array(2)].map((i, idx) => (
-                                <a-col {...{ sm: 14, md: 12, lg: 10, xl: 8 }}>
-                                    <Weather />
-                                </a-col>
-                            ))
-
-                        } */}
 
                         <Weather />
 
-                        <a-card class="card-new-city">
-                            <h2>Add city</h2>
+                        <NewCity />
 
-                            <a-button class="btn-add" block onClick={() => alert('aaa')} type="link" v-slots={{
-                                icon: () => <PlusCircleTwoTone style={{ fontSize: '4em' }} twoToneColor="#16d9ad" />
-                            }}>
-
-                            </a-button>
-                            <InlineSvg {...props} />
-
-                        </a-card>
                     </a-row>
-                    <Newsletters v-show={true} />
+                    <Newsletters />
                 </div>
             </div>
             // </a-config-provider>
--- src/components/newsletters/index.jsx
+++ src/components/newsletters/index.jsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onBeforeMount } from 'vue'
 import Avatar from '@/assets/avatar.jpg'
 import './index.less'
 
